f1338628cf7e18f97fb3122da494b4b73cca036d
stephendoty826 <stephendoty826@yahoo.com>
2025-03-12 20:20:30 -0500

    adding bff backend...using flask and python

--- bff/bff.py
+++ bff/bff.py
@@ -1,10 +1,19 @@
-from flask import Flask
+import requests
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
   return "HELLO"
+
+@app.route("/dbAuth")
+def db_auth():
+  redirect("https://www.dropbox.com/oauth2/authorize?client_id=6tp50cpnwmi7y1g&redirect_uri=http://127.0.0.1:5000/test_redirect&token_access_type=offline&response_type=code")
+
+@app.route("/test_redirect")
+def test_redirect():
+  
 
 if __name__=="__main__":
   app.run(debug=True)
