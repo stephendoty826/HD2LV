15459e7fc7cb86db95fdb1b7287eb5bfb75659ec
stephendoty826 <stephendoty826@yahoo.com>
2025-04-13 17:46:38 -0500

    added close button for closing details. Also fixed stratagems with short descriptions taking up less width.

--- src/components/StratagemInfoView.jsx
+++ src/components/StratagemInfoView.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { Container } from 'react-bootstrap'
+import Container from 'react-bootstrap/Container'
+import Button from "react-bootstrap/Button"
 import helldivers2Data from "../gameData/helldivers2.json";
 
 const StratagemInfoView = () => {
@@ -14,6 +15,12 @@ const StratagemInfoView = () => {
   const handleSelectStrat = (stratagem) => {
     setSelected(stratagem)
     setShowDetails(true)
+  }
+
+  //todo function to close details and deselect strat
+  const handleCloseDetails = () => {
+    setShowDetails(false)
+    setSelected({})
   }
 
   return (
@@ -46,98 +53,101 @@ const StratagemInfoView = () => {
           );
         })}
       </div>
-      <div className={showDetails ? "infoContainerBottom" : "modalBottomClosed"}>
-        <div className="d-flex align-items-center" style={{ padding: "0px" }}>
-          <img
-            src={selected.image}
-            alt=""
-            className="me-2"
-            style={{ height: "5vh" }}
-          />
-          <div className="d-flex">
-            <div className="fs-5">{selected.name?.toUpperCase()}</div>
-          </div>
-        </div>
-        <div>{selected.description}</div>
-        <div className="mt-2">
-          <div className="mx-2 fs-5">STATS</div>
-          <div className="px-2 infoBox">
-            {selected.damage && (
-              <div className="pt-1"><b>Damage:</b> {selected.damage}</div>
-            )}
-            {selected["damage/sec"] && (
-              <div className="pt-1"><b>Damage/sec:</b> {selected["damage/sec"]}</div>
-            )}
-            {selected["durability damage"] && (
-              <div className="pt-1"><b>Durability Damage</b> {selected["durability damage"]}</div>
-            )}
-            {selected["armor penetration"] && (
-              <div className="pt-1"><b>Armor Penetration:</b> {selected["armor penetration"]}</div>
-            )}
-            <div className="pt-1"><b>Call-in Time:</b> {selected["call-in time"]}</div>
-            <div className="pt-1"><b>Uses:</b> {selected.uses}</div>
-            {selected.capacity && (
-              <div className="pt-1"><b>Capacity:</b> {selected.capacity}</div>
-            )}
-            {selected["fire rate"] && (
-              <div className="pt-1">
-                <b>Fire Rate:</b> {selected["fire rate"]}
-              </div>
-            )}
-            {selected["charge time"] && (
-              <div className="pt-1"><b>Charge Time:</b> {selected["charge time"]}</div>
-            )}
-            {selected["recharge time"] && (
-              <div className="pt-1"><b>Recharge Time:</b> {selected["recharge time"]}</div>
-            )}
-            {selected["fire limit"] && (
-              <div className="pt-1"><b>Fire Limit:</b> {selected["fire limit"]}</div>
-            )}
-            {selected["spare magazines"] && (
-              <div className="pt-1">
-                <b>Spare Magazines:</b> {selected["spare magazines"]}
-              </div>
-            )}
-            {selected["reload time"] && (
-              <div className="pt-1"><b>Reload Time:</b> {selected["reload time"]}</div>
-            )}
-            {selected["tactical reload"] && (
-              <div className="pt-1">
-                <b>Tactical Reload:</b> {selected["tactical reload"]}
-              </div>
-            )}
-            {selected["rearm time"] && (
-              <div className="pt-1"><b>Rearm Time:</b> {selected["rearm time"]}</div>
-            )}
-            <div className="py-1">
-              <b>Cooldown Time:</b> {selected["cooldown time"]}
-            </div>
-            {selected["more info"] && (
-              <div className="pt-1">
-                <a href={selected["more info"]} target="_blank" rel="noreferrer">MORE INFO</a>
-              </div>
-            )}
-          </div>
-        </div>
-        <div className="mt-2">
-          <div className="mx-2 fs-5">STRATAGEM TRAITS</div>
-          <div className="px-2 infoBox">
-            <div className="pt-1">
-              <ul>
-                {selected["stratagem traits"] &&
-                  selected["stratagem traits"].map((trait, idx) => {
-                    return (
-                      <li key={idx} className="pb-1">
-                        {trait.toUpperCase()}
-                      </li>
-                    );
-                  })}
-              </ul>
+      <div className={showDetails ? "infoContainerBottom w-100" : "modalBottomClosed w-100"}>
+        <Button className="floating-btn" variant="dark" onClick={handleCloseDetails}>Close</Button>
+        <div className="scrollable-content">
+          <div className="d-flex align-items-center" style={{ padding: "0px" }}>
+            <img
+              src={selected.image}
+              alt=""
+              className="me-2"
+              style={{ height: "5vh" }}
+            />
+            <div className="d-flex">
+              <div className="fs-5 col-6 col-sm-12">{selected.name?.toUpperCase()}</div>
             </div>
           </div>
-          {selected.credit && <a href={selected.credit} target="_blank" rel="noreferrer">
-            Image credit
-          </a>}
+          <div>{selected.description}</div>
+          <div className="mt-2">
+            <div className="mx-2 fs-5">STATS</div>
+            <div className="px-2 infoBox">
+              {selected.damage && (
+                <div className="pt-1"><b>Damage:</b> {selected.damage}</div>
+              )}
+              {selected["damage/sec"] && (
+                <div className="pt-1"><b>Damage/sec:</b> {selected["damage/sec"]}</div>
+              )}
+              {selected["durability damage"] && (
+                <div className="pt-1"><b>Durability Damage</b> {selected["durability damage"]}</div>
+              )}
+              {selected["armor penetration"] && (
+                <div className="pt-1"><b>Armor Penetration:</b> {selected["armor penetration"]}</div>
+              )}
+              <div className="pt-1"><b>Call-in Time:</b> {selected["call-in time"]}</div>
+              <div className="pt-1"><b>Uses:</b> {selected.uses}</div>
+              {selected.capacity && (
+                <div className="pt-1"><b>Capacity:</b> {selected.capacity}</div>
+              )}
+              {selected["fire rate"] && (
+                <div className="pt-1">
+                  <b>Fire Rate:</b> {selected["fire rate"]}
+                </div>
+              )}
+              {selected["charge time"] && (
+                <div className="pt-1"><b>Charge Time:</b> {selected["charge time"]}</div>
+              )}
+              {selected["recharge time"] && (
+                <div className="pt-1"><b>Recharge Time:</b> {selected["recharge time"]}</div>
+              )}
+              {selected["fire limit"] && (
+                <div className="pt-1"><b>Fire Limit:</b> {selected["fire limit"]}</div>
+              )}
+              {selected["spare magazines"] && (
+                <div className="pt-1">
+                  <b>Spare Magazines:</b> {selected["spare magazines"]}
+                </div>
+              )}
+              {selected["reload time"] && (
+                <div className="pt-1"><b>Reload Time:</b> {selected["reload time"]}</div>
+              )}
+              {selected["tactical reload"] && (
+                <div className="pt-1">
+                  <b>Tactical Reload:</b> {selected["tactical reload"]}
+                </div>
+              )}
+              {selected["rearm time"] && (
+                <div className="pt-1"><b>Rearm Time:</b> {selected["rearm time"]}</div>
+              )}
+              <div className="py-1">
+                <b>Cooldown Time:</b> {selected["cooldown time"]}
+              </div>
+              {selected["more info"] && (
+                <div className="pt-1">
+                  <a href={selected["more info"]} target="_blank" rel="noreferrer">MORE INFO</a>
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="mt-2">
+            <div className="mx-2 fs-5">STRATAGEM TRAITS</div>
+            <div className="px-2 infoBox">
+              <div className="pt-1">
+                <ul>
+                  {selected["stratagem traits"] &&
+                    selected["stratagem traits"].map((trait, idx) => {
+                      return (
+                        <li key={idx} className="pb-1">
+                          {trait.toUpperCase()}
+                        </li>
+                      );
+                    })}
+                </ul>
+              </div>
+            </div>
+            {selected.credit && <a href={selected.credit} target="_blank" rel="noreferrer">
+              Image credit
+            </a>}
+          </div>
         </div>
       </div>
     </Container>
