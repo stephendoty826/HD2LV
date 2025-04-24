104c736574f37f181596f7ae1b1afae91d974a07
stephendoty826 <stephendoty826@yahoo.com>
2025-04-23 22:39:59 -0500

    Made a InfoTopView component to try and reduce the amount of repeating code. Also look into making an InfoViewBottom component to see if you can reduce the bottom part repeating code. WILO: need to incorporate InfoTopView on the other info views.

--- src/components/InfoViews/StratagemInfoView.jsx
+++ src/components/InfoViews/StratagemInfoView.jsx
@@ -1,83 +1,47 @@
-import React, { useState, useRef, useEffect } from 'react'
-import Container from 'react-bootstrap/Container'
-import Button from "react-bootstrap/Button"
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 import helldivers2Data from "../../gameData/helldivers2.json";
-import { scrollToItem } from "../../misc/utils";
 import { MoreInfoJSX, ImageCreditJSX } from "../SubComponents/SelectorMisc";
+import InfoViewTop from "./InfoViewTop";
 
 const StratagemInfoView = () => {
+  const [selected, setSelected] = useState({});
+  const [showDetails, setShowDetails] = useState(false);
 
-  const [selected, setSelected] = useState({})
-  const [showDetails, setShowDetails] = useState(false)
-  const stratagemRefs = useRef([])
-
-  let flatIndex = 0; // index for scrolling to stratagem when clicked
-
-  let stratagemObj = helldivers2Data.stratagems
-  
-  let keysArray = Object.keys(stratagemObj);
+  let stratagemObj = helldivers2Data.stratagems;
 
   const handleSelectStrat = (stratagem) => {
-    setSelected(stratagem)
-    setShowDetails(true)
-  }
+    setSelected(stratagem);
+    setShowDetails(true);
+  };
 
   const handleCloseDetails = () => {
-    setShowDetails(false)
-    setSelected({})
-  }
-
-  useEffect(() => {
-    if(showDetails && selected.name){
-
-      const flatStratArr = keysArray.flatMap(key => stratagemObj[key]);
-
-      const index = flatStratArr.findIndex(strat => strat.name === selected.name)
-
-      if(index !== -1){
-        const element = stratagemRefs.current[index]
-        scrollToItem(element)
-      }
-    }
-
-  }, [showDetails, selected.name, keysArray, stratagemObj])
+    setShowDetails(false);
+    setSelected({});
+  };
 
   return (
     <Container className="d-flex flex-column align-items-center">
-      <div className={showDetails ? "infoContainerWithDetails" : "infoContainer"}>
-        {keysArray.map((stratagemKey, idx) => {
-          return (
-            <div key={stratagemKey + idx}>
-              <p>{stratagemKey.toUpperCase()}</p>
-              <div className="row">
-                {stratagemObj[stratagemKey].map((stratagem) => {
-                  let isSelected = selected?.name === stratagem.name;
-                  const refIndex = flatIndex; // capture current flat index
-                  flatIndex++ // increment for next one
-
-                  return (
-                    <div className="col-3" key={stratagem.image}>
-                      <img
-                        className={
-                          isSelected
-                            ? "selected itemSelector"
-                            : "itemSelector"
-                        }
-                        src={stratagem.image}
-                        alt=""
-                        ref={(el) => (stratagemRefs.current[refIndex] = el)}
-                        onClick={() => handleSelectStrat(stratagem)}
-                      />
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <div className={showDetails ? "infoContainerBottom w-100" : "modalBottomClosed w-100"}>
-        <Button className="floating-btn" variant="dark" onClick={handleCloseDetails}>Close</Button>
+      <InfoViewTop 
+        selected={selected}
+        showDetails={showDetails}
+        handleSelectItem={handleSelectStrat}
+        itemsObj={stratagemObj}
+        itemsPerRow={4}
+      />
+      <div
+        className={
+          showDetails ? "infoContainerBottom w-100" : "modalBottomClosed w-100"
+        }
+      >
+        <Button
+          className="floating-btn"
+          variant="dark"
+          onClick={handleCloseDetails}
+        >
+          Close
+        </Button>
         <div className="scrollable-content">
           <div className="d-flex align-items-center" style={{ padding: "0px" }}>
             <img
@@ -87,7 +51,9 @@ const StratagemInfoView = () => {
               style={{ height: "5vh" }}
             />
             <div className="d-flex">
-              <div className="fs-5 col-6 col-sm-12">{selected.name?.toUpperCase()}</div>
+              <div className="fs-5 col-6 col-sm-12">
+                {selected.name?.toUpperCase()}
+              </div>
             </div>
           </div>
           <div>{selected.description}</div>
@@ -95,21 +61,35 @@ const StratagemInfoView = () => {
             <div className="mx-2 fs-5">STATS</div>
             <div className="px-2 infoBox">
               {selected.damage && (
-                <div className="pt-1"><b>Damage:</b> {selected.damage}</div>
+                <div className="pt-1">
+                  <b>Damage:</b> {selected.damage}
+                </div>
               )}
               {selected["damage/sec"] && (
-                <div className="pt-1"><b>Damage/sec:</b> {selected["damage/sec"]}</div>
+                <div className="pt-1">
+                  <b>Damage/sec:</b> {selected["damage/sec"]}
+                </div>
               )}
               {selected["durability damage"] && (
-                <div className="pt-1"><b>Durability Damage</b> {selected["durability damage"]}</div>
+                <div className="pt-1">
+                  <b>Durability Damage</b> {selected["durability damage"]}
+                </div>
               )}
               {selected["armor penetration"] && (
-                <div className="pt-1"><b>Armor Penetration:</b> {selected["armor penetration"]}</div>
+                <div className="pt-1">
+                  <b>Armor Penetration:</b> {selected["armor penetration"]}
+                </div>
               )}
-              <div className="pt-1"><b>Call-in Time:</b> {selected["call-in time"]}</div>
-              <div className="pt-1"><b>Uses:</b> {selected.uses}</div>
+              <div className="pt-1">
+                <b>Call-in Time:</b> {selected["call-in time"]}
+              </div>
+              <div className="pt-1">
+                <b>Uses:</b> {selected.uses}
+              </div>
               {selected.capacity && (
-                <div className="pt-1"><b>Capacity:</b> {selected.capacity}</div>
+                <div className="pt-1">
+                  <b>Capacity:</b> {selected.capacity}
+                </div>
               )}
               {selected["fire rate"] && (
                 <div className="pt-1">
@@ -117,13 +97,19 @@ const StratagemInfoView = () => {
                 </div>
               )}
               {selected["charge time"] && (
-                <div className="pt-1"><b>Charge Time:</b> {selected["charge time"]}</div>
+                <div className="pt-1">
+                  <b>Charge Time:</b> {selected["charge time"]}
+                </div>
               )}
               {selected["recharge time"] && (
-                <div className="pt-1"><b>Recharge Time:</b> {selected["recharge time"]}</div>
+                <div className="pt-1">
+                  <b>Recharge Time:</b> {selected["recharge time"]}
+                </div>
               )}
               {selected["fire limit"] && (
-                <div className="pt-1"><b>Fire Limit:</b> {selected["fire limit"]}</div>
+                <div className="pt-1">
+                  <b>Fire Limit:</b> {selected["fire limit"]}
+                </div>
               )}
               {selected["spare magazines"] && (
                 <div className="pt-1">
@@ -131,7 +117,9 @@ const StratagemInfoView = () => {
                 </div>
               )}
               {selected["reload time"] && (
-                <div className="pt-1"><b>Reload Time:</b> {selected["reload time"]}</div>
+                <div className="pt-1">
+                  <b>Reload Time:</b> {selected["reload time"]}
+                </div>
               )}
               {selected["tactical reload"] && (
                 <div className="pt-1">
@@ -139,12 +127,14 @@ const StratagemInfoView = () => {
                 </div>
               )}
               {selected["rearm time"] && (
-                <div className="pt-1"><b>Rearm Time:</b> {selected["rearm time"]}</div>
+                <div className="pt-1">
+                  <b>Rearm Time:</b> {selected["rearm time"]}
+                </div>
               )}
               <div className="py-1">
                 <b>Cooldown Time:</b> {selected["cooldown time"]}
               </div>
-              <MoreInfoJSX selected={selected}/>
+              <MoreInfoJSX selected={selected} />
             </div>
           </div>
           <div className="mt-2">
@@ -164,11 +154,11 @@ const StratagemInfoView = () => {
               </div>
             </div>
           </div>
-          <ImageCreditJSX selected={selected}/>
+          <ImageCreditJSX selected={selected} />
         </div>
       </div>
     </Container>
   );
-}
+};
 
-export default StratagemInfoView
+export default StratagemInfoView;
--- src/components/InfoViews/WeaponsInfoView.jsx
+++ src/components/InfoViews/WeaponsInfoView.jsx
@@ -1,17 +1,13 @@
-import React, { useState, useRef, useEffect }  from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import { scrollToItem } from "../../misc/utils";
 import { MoreInfoJSX, ImageCreditJSX } from "../SubComponents/SelectorMisc";
+import InfoViewTop from "./InfoViewTop";
 
-const WeaponsInfoView = ({weaponsObj}) => {
+const WeaponsInfoView = ({ weaponsObj }) => {
   const [selected, setSelected] = useState({});
   const [showDetails, setShowDetails] = useState(false);
-  const weaponRefs = useRef([])
-
-  let flatIndex = 0; // index for scrolling to armor when clicked
-
-  let keysArray = Object.keys(weaponsObj);
 
   const handleSelectWeapon = (weapon) => {
     setSelected(weapon);
@@ -23,56 +19,15 @@ const WeaponsInfoView = ({weaponsObj}) => {
     setSelected({});
   };
 
-  useEffect(() => {
-    if(showDetails && selected.name){
-
-      const flatWeaponsArr = keysArray.flatMap(key => weaponsObj[key]);
-
-      const index = flatWeaponsArr.findIndex(weapon => weapon.name === selected.name)
-
-      if(index !== -1){
-        const element = weaponRefs.current[index]
-        scrollToItem(element)
-      }
-    }
-
-  }, [showDetails, selected.name, keysArray, weaponsObj])
-
   return (
     <Container className="d-flex flex-column align-items-center">
-      <div
-        className={showDetails ? "infoContainerWithDetails" : "infoContainer"}
-      >
-        {keysArray.map((weaponKey, idx) => {
-          return (
-            <div key={weaponKey + idx}>
-              <p>{weaponKey.toUpperCase()}</p>
-              <div className="row">
-                {weaponsObj[weaponKey].map((weapon) => {
-                  let isSelected = selected.name === weapon.name;
-                  const refIndex = flatIndex; // capture current flat index
-                  flatIndex++ // increment for next one
-
-                  return (
-                    <div className="col-6" key={weapon.image}>
-                      <img
-                        className={
-                          isSelected ? "selected itemSelector" : "itemSelector"
-                        }
-                        src={weapon.image}
-                        alt=""
-                        ref={(el) => (weaponRefs.current[refIndex] = el)}
-                        onClick={() => handleSelectWeapon(weapon)}
-                      />
-                    </div>
-                  );
-                })}
-              </div>
-              <hr />
-            </div>
-          );
-        })}
-      </div>
+      <InfoViewTop 
+        selected={selected}
+        showDetails={showDetails}
+        handleSelectItem={handleSelectWeapon}
+        itemsObj={weaponsObj}
+        itemsPerRow={2}
+      />
       <div
         className={
           showDetails ? "infoContainerBottom w-100" : "modalBottomClosed w-100"
@@ -104,28 +59,42 @@ const WeaponsInfoView = ({weaponsObj}) => {
             <div className="mx-2 fs-5">STATS</div>
             <div className="px-2 infoBox">
               {selected.damage && (
-                <div className="pt-1"><b>Damage:</b> {selected.damage}</div>
+                <div className="pt-1">
+                  <b>Damage:</b> {selected.damage}
+                </div>
               )}
               {selected["damage/sec"] && (
-                <div className="pt-1"><b>Damage/sec:</b> {selected["damage/sec"]}</div>
+                <div className="pt-1">
+                  <b>Damage/sec:</b> {selected["damage/sec"]}
+                </div>
               )}
               {selected["durability damage"] && (
-                <div className="pt-1"><b>Durability Damage:</b> {selected["durability damage"]}</div>
+                <div className="pt-1">
+                  <b>Durability Damage:</b> {selected["durability damage"]}
+                </div>
               )}
               {selected["armor penetration"] && (
-                <div className="pt-1"><b>Armor Penetration:</b> {selected["armor penetration"]}</div>
+                <div className="pt-1">
+                  <b>Armor Penetration:</b> {selected["armor penetration"]}
+                </div>
               )}
               {selected.capacity && (
-                <div className="pt-1"><b>Capacity:</b> {selected.capacity}</div>
+                <div className="pt-1">
+                  <b>Capacity:</b> {selected.capacity}
+                </div>
               )}
-              <div className="py-1"><b>Recoil:</b> {selected.recoil}</div>
+              <div className="py-1">
+                <b>Recoil:</b> {selected.recoil}
+              </div>
               {selected["fire rate"] && (
                 <div className="pt-1">
                   <b>Fire Rate (rpm):</b> {selected["fire rate"]}
                 </div>
               )}
               {selected["fire limit"] && (
-                <div className="pt-1"><b>Fire Limit:</b> {selected["fire limit"]}</div>
+                <div className="pt-1">
+                  <b>Fire Limit:</b> {selected["fire limit"]}
+                </div>
               )}
               {selected["spare magazines"] && (
                 <div className="pt-1">
@@ -133,14 +102,16 @@ const WeaponsInfoView = ({weaponsObj}) => {
                 </div>
               )}
               {selected["reload time"] && (
-                <div className="pt-1"><b>Reload Time:</b> {selected["reload time"]}</div>
+                <div className="pt-1">
+                  <b>Reload Time:</b> {selected["reload time"]}
+                </div>
               )}
               {selected["tactical reload"] && (
                 <div className="pt-1">
                   <b>Tactical Reload:</b> {selected["tactical reload"]}
                 </div>
               )}
-              <MoreInfoJSX selected={selected}/>
+              <MoreInfoJSX selected={selected} />
             </div>
           </div>
           <div className="mt-2">
@@ -162,7 +133,7 @@ const WeaponsInfoView = ({weaponsObj}) => {
                 </div>
               </div>
             )}
-            <ImageCreditJSX selected={selected}/>
+            <ImageCreditJSX selected={selected} />
           </div>
         </div>
       </div>
